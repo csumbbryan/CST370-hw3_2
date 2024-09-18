dcecad1d7950e300f86144783027ab946936d7a3
csumbbryan <bzanoli@csumb.edu>
2024-09-17 17:29:58 -0700

    Minor update to output formatting

--- Main.java
+++ Main.java
@@ -167,7 +167,7 @@ class Main
             } else {
                 paths += "\n";
             }
-            paths += "Cost:" + this.minPath + "\n";
+            paths += "Cost:" + this.minPath;
             return paths;
         }
     }
